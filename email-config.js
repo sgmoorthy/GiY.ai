386c4d3100facbc589b785a0d7ded2a5ff9fe135
Gurumurthy Swaminathan <sgmoorthy@gmail.com>
2025-03-13 18:09:48 +0530

    modified with new features

---
+++ email-config.js
@@ -0,0 +1,6 @@
+// EmailJS configuration
+const PUBLIC_KEY = 'EoG4rC9jMQrfveYgd';
+const TEMPLATE_ID = 'template_kw37u0c';
+const SERVICE_ID = 'service_e8200dn';
+
+export { PUBLIC_KEY, TEMPLATE_ID, SERVICE_ID };
